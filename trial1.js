4d756025380df9cc7034e1cb53a1fdabf223fb40
gsskiran7197 <107458098+gsskiran7197@users.noreply.github.com>
2024-02-18 15:26:49 +0530

    Update trial1.js

--- trial1.js
+++ trial1.js
@@ -1,7 +1,7 @@
 let array =[1,2,3,4,5,6];
 
 let outputArray=array.map(function(a,b){
-  console.log(b);           // a is pointing the array values and b their index.
+  console.log(b);           
   return a*2;
 });
 
